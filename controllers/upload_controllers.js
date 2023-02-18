8ca7cf87029f280c569f90eef91640e2db6a7020
santiagoTrivi <santiagocarvajal103@gmail.com>
2023-02-17 22:50:23 -0400

    last part

--- controllers/upload_controllers.js
+++ controllers/upload_controllers.js
@@ -1,3 +1,5 @@
+const  path  = require('path');
+const fs = require('fs');
 const { request, response } = require("express");
 const { uploadFiles } = require("../helpers");
 const { User } = require("../models/userData");
@@ -48,6 +50,13 @@ const imgUpdate = async (req = request, res = response) =>{
  
             
     }
+
+    if (modelo.img){
+        const pathFile = path.join(__dirname, '../uploads', collection, modelo.img);
+        if(fs.existsSync(pathFile)){
+            fs.unlinkSync(pathFile);
+        };
+    };
     
     const fileName = await uploadFiles(req.files, undefined, collection);
     modelo.img = fileName;
@@ -59,9 +68,53 @@ const imgUpdate = async (req = request, res = response) =>{
 };
 
 
+const getPicture = async(req = request, res = response) =>{
 
+    const {collection, id} = req.params;
+    let modelo;
+
+    switch (collection) {
+        case 'user':
+            modelo = await User.findByPk(id);
+
+            if(!modelo){
+                return res.status(400).json({message: `the users ${id} does not exist`});
+            }
+
+            break;
+        case 'product':
+            modelo = await Product.findByPk(id);
+
+            if(!modelo){
+                return res.status(400).json({message: `the product ${id} does not exist`});
+            }
+
+            break;
+        
+        default:
+            return res.status(500).json({message: 'part not developed'})
+ 
+            
+    }
+
+    if (modelo.img){
+        const pathFile = path.join(__dirname, '../uploads', collection, modelo.img);
+        if(fs.existsSync(pathFile)){
+            return res.sendFile(pathFile);
+        };
+    };
+
+
+    const pathFileNoFound = path.join(__dirname, '../assets', 'index.jpg');
+    if(fs.existsSync(pathFileNoFound)){
+        return res.sendFile(pathFileNoFound);
+    };
+
+    res.json({message: 'error'});
+};
 
 module.exports = {
     uploadfile,
-    imgUpdate
+    imgUpdate,
+    getPicture
 };
--- routes/upload_routes.js
+++ routes/upload_routes.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const {uploadfile, imgUpdate} = require('../controllers/upload_controllers');
+const {uploadfile, imgUpdate, getPicture} = require('../controllers/upload_controllers');
 const { allowedCollections } = require('../helpers');
 const { fieldCheck, checkFile } = require('../middlewares');
 const router = Router();
@@ -9,5 +9,6 @@ router.post('/', checkFile, uploadfile);
 
 router.put('/:collection/:id', [checkFile , check('collection').custom( c => allowedCollections( c, ['user', 'product']))], fieldCheck ,imgUpdate);
 
+router.get('/:collection/:id', [check('collection').custom( c => allowedCollections( c, ['user', 'product']))], fieldCheck, getPicture);
 
 module.exports = router;
